4aa1901d32ee2aee7593fa7f9510a84fd886a1f8
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-08-04 21:48:28 +0200

    objects/JS

--- index.js
+++ index.js
@@ -22,8 +22,44 @@ const product3 = {
 const firstProductCart = document.body.querySelector('#product1');
 firstProductCart.classList.add('border-primary');
 
-const secondProductButton = document.body.querySelector('#product2 button');
-secondProductButton.classList.remove('btn-primary');
+/*const secondProductButton = document.body.querySelector('#product2 button');
+secondProductButton.classList.remove('btn-primary');*/
 
-const thirdProductCardTitle = document.querySelector('#product3 .card-title');
-thirdProductCardTitle.classList.toggle('text-center');
+/*const thirdProductCardTitle = document.querySelector('#product3 card-title');
+thirdProductCardTitle.classList.toggle('text-center');*/
+
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML = product1HTML;
+
+const product2HTML = `
+<img class="card-img-top" src="images/slepickabelka.jpg" alt="Slepičkabelka">
+  <div class="card-body">
+    <h2 class="card-title">Slepičkabelka</h2>
+    <p class="card-text">Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product2Elm = document.querySelector('#product2');
+product2Elm.innerHTML = product2HTML;
+
+const product3HTML = `
+<img class="card-img-top" src="images/anatoplavky.jpg" alt="Anatoplavky">
+  <div class="card-body">
+   <h2 class="card-title">Anatoplavky</h2>
+   <p class="card-text">Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.</p>
+   </div>
+<button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product3Elm = document.querySelector('#product3');
+product3Elm.innerHTML = product3HTML;
