62b33e11fd616ab270b4a8c2a1fc04452268031d
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-08-05 20:20:01 +0200

    button + classes

--- index.js
+++ index.js
@@ -19,15 +19,6 @@ const product3 = {
     'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
-const firstProductCart = document.body.querySelector('#product1');
-firstProductCart.classList.add('border-primary');
-
-/*const secondProductButton = document.body.querySelector('#product2 button');
-secondProductButton.classList.remove('btn-primary');*/
-
-/*const thirdProductCardTitle = document.querySelector('#product3 card-title');
-thirdProductCardTitle.classList.toggle('text-center');*/
-
 const product1HTML = `
   <img class="card-img-top" src="${product1.image}" alt="Card image cap">
   <div class="card-body">
@@ -41,25 +32,40 @@ const product1Elm = document.querySelector('#product1');
 product1Elm.innerHTML = product1HTML;
 
 const product2HTML = `
-<img class="card-img-top" src="images/slepickabelka.jpg" alt="Slepičkabelka">
+<img class="card-img-top" src="${product2.image}" alt="Slepičkabelka">
   <div class="card-body">
-    <h2 class="card-title">Slepičkabelka</h2>
-    <p class="card-text">Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.</p>
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
   </div>
-  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Není skladem</button>
 `;
 
 const product2Elm = document.querySelector('#product2');
 product2Elm.innerHTML = product2HTML;
 
 const product3HTML = `
-<img class="card-img-top" src="images/anatoplavky.jpg" alt="Anatoplavky">
+<img class="card-img-top" src="${product3.image}" alt="Anatoplavky">
   <div class="card-body">
-   <h2 class="card-title">Anatoplavky</h2>
-   <p class="card-text">Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.</p>
+   <h2 class="card-title">${product3.name}</h2>
+   <p class="card-text">${product3.description}</p>
    </div>
 <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
 `;
 
 const product3Elm = document.querySelector('#product3');
 product3Elm.innerHTML = product3HTML;
+
+const firstProductCart = document.querySelector('#product1');
+firstProductCart.classList.add('border-primary');
+
+const secondProductButton = document.querySelector('#product2 button');
+secondProductButton.classList.remove('btn-primary');
+
+const firstProductCardTitle = document.querySelector('#product1 .card-title');
+firstProductCardTitle.classList.toggle('text-center');
+
+const secondProductCardTitle = document.querySelector('#product2 .card-title');
+secondProductCardTitle.classList.toggle('text-center');
+
+const thirdProductCardTitle = document.querySelector('#product3 .card-title');
+thirdProductCardTitle.classList.toggle('text-center');
